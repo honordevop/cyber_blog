b5e504f83892f99b6388deb9681d7f104351b3be
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-09-06 13:47:57 +0100

    Code: code file structure established

---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./darkmodetoggle.module.css";
+
+const DarkModeToggle = () => {
+  return <div>DarkModeToggle</div>;
+};
+
+export default DarkModeToggle;
